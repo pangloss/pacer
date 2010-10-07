2dfef82968c8a86c505356e2118dd1c075a2b35e
Darrick Wiebe <darrick@innatesoftware.com>
2010-10-07 11:50:33 -0400

    Import and export graphml.

--- lib/pacer/graph.rb
+++ lib/pacer/graph.rb
@@ -1,6 +1,24 @@
 module Pacer
-  import com.tinkerpop.blueprints.pgm.Graph;
+  import com.tinkerpop.blueprints.pgm.Graph
 
+  module Graph
+    def import(path)
+      path = File.expand_path path
+      begin
+        stream = java.net.URL.new(path).open_stream
+      rescue java.net.MalformedURLException
+        stream = java.io.FileInputStream.new path
+      end
+      com.tinkerpop.blueprints.pgm.parser.GraphMLReader.input_graph self, stream
+      true
+    end
+
+    def export(path)
+      path = File.expand_path path
+      stream = new java.io.FileOutputStream.new path
+      com.tinkerpop.blueprints.pgm.parser.GraphMLWriter.outputGraph self, stream
+    end
+  end
 
   module VertexMixin
     def inspect
