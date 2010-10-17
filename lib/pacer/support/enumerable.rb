6e580c83977a9a5671e48badf79edeb390fdcdc4
Darrick Wiebe <darrick@innatesoftware.com>
2010-10-17 13:54:35 -0400

    Improve to_hashset

--- lib/pacer/support/enumerable.rb
+++ lib/pacer/support/enumerable.rb
@@ -2,7 +2,28 @@
 module Enumerable
 
   # Transform the enumerable into a java HashSet.
-  def to_hashset
-    inject(java.util.HashSet.new) { |hs, e| hs.add e; hs }
+  def to_hashset(method = nil, *args)
+    hs = java.util.HashSet.new
+    iter = self.each
+    e = iter.next
+    if method
+      while true
+        hs.add e.send(method, *args)
+        e = iter.next
+      end
+    else
+      while true
+        hs.add e
+        e = iter.next
+      end
+    end
+  rescue StopIteration
+    return hs
+  rescue NativeException => e
+    if (e.cause.kind_of?(java.util.NoSuchElementException))
+      return hs
+    else
+      raise
+    end
   end
 end
