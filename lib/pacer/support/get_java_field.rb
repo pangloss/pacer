efd823a3498fdfe4a4b997fc259be4a02674a97f
Darrick Wiebe <darrick@innatesoftware.com>
2010-11-20 18:46:07 -0500

    Don't know if this is needed but it looks useful.

--- lib/pacer/support/get_java_field.rb
+++ lib/pacer/support/get_java_field.rb
@@ -7,5 +7,11 @@ module Pacer::Support
       field.accessible = true
       field.value Java.ruby_to_java(self)
     end
+
+    def set_java_field(name, value)
+      field = self.class.superclass.java_class.declared_field name
+      field.accessible = true
+      field.set Java.ruby_to_java(self), value
+    end
   end
 end
