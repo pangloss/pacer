6bbbf0798180e8cdb004a75eb8f0f245a7981dbc
Darrick Wiebe <darrick@innatesoftware.com>
2010-12-04 03:25:15 -0500

    Refactor.

--- lib/pacer/route/indexed_edges_route.rb
+++ lib/pacer/route/indexed_edges_route.rb
@@ -1,24 +1,6 @@
 module Pacer::Routes
   class IndexedEdgesRoute < EdgesRoute
-    def initialize(index, key, value, filters, block)
-      filters = Pacer::Helpers.extract_route_conditions(filters)
-      if filters.count == 1 and block.nil?
-        # indexed count is possible if no other conditions
-        @index = index
-        @key = key
-        @value = value
-      end
-      filters = Pacer::Helpers.remove_filter_key(filters, key)
-      initialize_path(proc { r = index.get(key, value); r ? r.iterator : [] }, filters, block)
-    end
-
-    def count
-      if @index and @key and @value
-        @index.count(@key, @value)
-      else
-        super
-      end
-    end
+    include IndexedRouteModule
 
     protected
 
--- lib/pacer/support.rb
+++ lib/pacer/support.rb
@@ -5,4 +5,3 @@ end
 require 'pacer/support/array_list'
 require 'pacer/support/get_java_field'
 require 'pacer/support/enumerable'
-require 'pacer/support/helpers'
