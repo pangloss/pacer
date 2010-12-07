9e10f9d5dd96f790aea75bae6337513342afe19d
Darrick Wiebe <darrick@innatesoftware.com>
2010-12-07 15:56:52 -0500

    Track which graphs are currently in a transaction.

--- lib/pacer/graph/transactions.rb
+++ lib/pacer/graph/transactions.rb
@@ -1,6 +1,10 @@
 module Pacer
   import com.tinkerpop.blueprints.pgm.TransactionalGraph
 
+  def self.graphs_in_transaction
+    @graphs ||= Set[]
+  end
+
   module Graph
     def manual_transaction
       manual_transactions do
@@ -14,9 +18,11 @@ module Pacer
       original_mode = get_transaction_mode
       if original_mode != TransactionalGraph::Mode::MANUAL
         begin
+          puts "transaction mode reset to MANUAL" if Pacer.verbose == :very
           set_transaction_mode TransactionalGraph::Mode::MANUAL
           yield
         ensure
+          puts "transaction mode reset to #{ original_mode }" if Pacer.verbose == :very
           set_transaction_mode original_mode
         end
       else
@@ -25,6 +31,7 @@ module Pacer
     end
 
     def transaction
+      Pacer.graphs_in_transaction << self
       start_transaction
       conclusion = TransactionalGraph::Conclusion::FAILURE
       begin
@@ -32,22 +39,35 @@ module Pacer
           yield
           conclusion = TransactionalGraph::Conclusion::SUCCESS
         end
+      rescue IRB::Abort
+        puts "transaction aborted" if Pacer.verbose == :very
       ensure
+        puts "transaction finished #{ conclusion }" if Pacer.verbose == :very
         stop_transaction conclusion
+        Pacer.graphs_in_transaction.delete self
       end
     end
 
+    def start_transaction
+      startTransaction
+    end
+
     def commit_transaction
-      stop_transaction TransactionalGraph::Conclusion::SUCCESS
+      r = stop_transaction TransactionalGraph::Conclusion::SUCCESS
+      Pacer.graphs_in_transaction.delete self
+      r
     end
 
     def rollback_transaction
-      stop_transaction TransactionalGraph::Conclusion::FAILURE
+      r = stop_transaction TransactionalGraph::Conclusion::FAILURE
+      Pacer.graphs_in_transaction.delete self
+      r
     end
 
     def checkpoint
       commit_transaction
       start_transaction
+      Pacer.graphs_in_transaction << self
     end
   end
 end
