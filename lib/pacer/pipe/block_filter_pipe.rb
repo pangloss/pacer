2d50391a938cec1da5e65199e2dc81a976f31e12
Darrick Wiebe <darrick@innatesoftware.com>
2010-12-06 23:47:19 -0500

    Extend elements as they're iterated over by a block. I optimized this to precalculate which extensions actually get applied. Hopefully package_extensions can also be used everywhere else.

--- lib/pacer/pipe/block_filter_pipe.rb
+++ lib/pacer/pipe/block_filter_pipe.rb
@@ -5,12 +5,30 @@ module Pacer::Pipes
       set_starts(starts)
       @back = back
       @block = block
+
+      @extensions = @back.extensions
+      @extension = package_extensions @back, @back.extensions
+    end
+
+    def package_extensions(route, extensions)
+      extension = Module.new
+      extensions.each do |mod|
+        extension.send :include, mod::Route if mod.const_defined? :Route
+        if route.is_a? Pacer::Routes::VerticesRouteModule
+          extension.send(:include, mod::Vertex) if mod.const_defined? :Vertex
+        elsif route.is_a? Pacer::Routes::EdgeRouteModule
+          extension.send(:include, mod::Edge) if mod.const_defined? :Edge
+        end
+      end
+      extension
     end
 
     def processNextStart()
       while s = @starts.next
         s.extend Pacer::Routes::SingleRoute
         s.back = @back
+        s.extensions.replace @extensions
+        s.extend @extension
         ok = @block.call s
         return s if ok
       end
