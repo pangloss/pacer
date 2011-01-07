cbc260af2a0eaaa1ad92fd517a0dbe564e85501a
Darrick Wiebe <darrick@innatesoftware.com>
2011-01-07 01:03:19 -0500

    Recovered some lost updates to TSort...

--- lib/pacer/utils/tsort.rb
+++ lib/pacer/utils/tsort.rb
@@ -5,7 +5,7 @@ module Pacer
     module TSort
       module Graph
         def tsort_dependencies_route
-          @tsort_dependencies_route ||= proc { |vertices| vertices.in_e.out_v }
+          @tsort_dependencies_route ||= proc { |vertices| vertices.in_e.out_v(Pacer::Utils::TSort) }
         end
 
         def tsort_dependencies_route=(callable)
@@ -19,7 +19,7 @@ module Pacer
         def tsort_dependencies_route
           return @tsort_dependencies_route if defined? @tsort_dependencies_route
           unless graph.respond_to? :tsort_dependencies_route
-            graph.extend TSort::Graph
+            graph.extend Pacer::Utils::TSort::Graph
           end
           @tsort_dependencies_route ||= graph.tsort_dependencies_route
         end
@@ -35,16 +35,19 @@ module Pacer
         end
 
         def tsort_each_child(node)
-          tsort_dependencies_route.call(node).each do |vertex|
+          node.tsort_dependencies.each do |vertex|
             yield vertex
           end
         end
       end
 
       module Vertex
-
         def tsort_each_node
           yield self
+        end
+
+        def tsort_dependencies
+          tsort_dependencies_route.call(self)
         end
       end
     end
