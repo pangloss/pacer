f6c8dce03062a7d93e328467d804d3a56c876879
Darrick Wiebe <darrick@innatesoftware.com>
2011-01-09 00:00:37 -0500

    Get rid of property filter methods in Base.

--- lib/pacer/route/mixin/base.rb
+++ lib/pacer/route/mixin/base.rb
@@ -482,42 +482,6 @@ module Pacer
         s
       end
 
-      def expand_extension_conditions(pipe, args_array)
-        modules = args_array.select { |obj| obj.is_a? Module or obj.is_a? Class }
-        pipe = modules.inject(pipe) do |p, mod|
-          if mod.respond_to? :route_conditions
-            args_array = args_array + [*mod.route_conditions]
-            p
-          elsif mod.respond_to? :route
-            route = mod.route(self)
-            beginning_of_condition = route.send :route_after, self
-            beginning_of_condition.send :source=, pipe
-            route.send :iterator
-          else
-            pipe
-          end
-        end
-        [pipe, args_array]
-      end
-
-      # Appends the defined filter pipes to narrow the results passed through
-      # the pipes for this route object.
-      def filter_pipe(pipe, args_array, block, expand_extensions)
-        if args_array and args_array.any?
-          pipe, args_array = expand_extension_conditions(pipe, args_array) if expand_extensions
-          pipe = args_array.select { |arg| arg.is_a? Hash }.inject(pipe) do |p, hash|
-            hash.inject(p) do |p2, (key, value)|
-              new_pipe = Pacer::Pipes::PropertyFilterPipe.new(key.to_s, value.to_java, Pacer::Pipes::ComparisonFilterPipe::Filter::NOT_EQUAL)
-              new_pipe.set_starts p2
-              new_pipe
-            end
-          end
-        end
-        if block
-          pipe = Pacer::Pipes::BlockFilterPipe.new(pipe, self, block)
-        end
-        pipe
-      end
     end
   end
 end
