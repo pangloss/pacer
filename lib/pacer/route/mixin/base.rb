558fa3e70cb78a5c2ef5771be1b3758c407628b9
Darrick Wiebe <darrick@innatesoftware.com>
2011-01-09 18:48:50 -0500

    This workaround was required to get paths working. I am not sure why the getPath and enablePath methods are being called on the source iterator of the pipeline...

--- lib/pacer/route/mixin/base.rb
+++ lib/pacer/route/mixin/base.rb
@@ -363,7 +363,12 @@ module Pacer
       # Return an iterator for a variety of source object types.
       def iterator_from_source(src)
         if src.is_a? Pacer::GraphMixin
-          [src].to_enum
+          i = [src].to_enum
+          # NOTE: This is a temporary workaround. I think there is a bug
+          # in pipes that causes this to be required?
+          def i.enablePath; end
+          def i.getPath; []; end
+          i
         elsif src.is_a? Pacer::ElementWrapper
           Pacer::Pipes::EnumerablePipe.new src.element
         elsif src.is_a? Proc
