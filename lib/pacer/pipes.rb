3e24c91d0479d6e35c1a478605a7b7979bf7b098
Darrick Wiebe <darrick@innatesoftware.com>
2011-01-21 01:40:32 -0500

    Created and added support for FutureOrFilterPipe.

--- lib/pacer/pipes.rb
+++ lib/pacer/pipes.rb
@@ -12,6 +12,7 @@ module Pacer
     import com.tinkerpop.pipes.filter.ComparisonFilterPipe
     import com.tinkerpop.pipes.filter.CollectionFilterPipe
     import com.tinkerpop.pipes.filter.FutureFilterPipe
+    import com.tinkerpop.pipes.filter.FutureOrFilterPipe
 
     import com.tinkerpop.pipes.pgm.PropertyFilterPipe
     import com.tinkerpop.pipes.pgm.GraphElementPipe
