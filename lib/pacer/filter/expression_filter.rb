13af2afad6cbda0e86c2789be048913b3f01e664
Darrick Wiebe <darrick@innatesoftware.com>
2011-01-31 03:43:30 -0500

    clean up double nesting of :string and extra space after :op.

--- lib/pacer/filter/expression_filter.rb
+++ lib/pacer/filter/expression_filter.rb
@@ -31,8 +31,8 @@ module Pacer
 
         rule(:integer)    { match('[0-9]').repeat(1).as(:int) >> space? }
         rule(:float)      { (match('[0-9]').repeat(1) >> str('.') >> match('[0-9]').repeat(1) ).as(:float) >> space? }
-        rule(:dq_string) { (str('"') >> ( str('\\') >> any | str('"').absnt? >> any ).repeat.as(:string) >> str('"')).as(:string) >> space? }
-        rule(:sq_string) { (str("'") >> ( str('\\') >> any | str("'").absnt? >> any ).repeat.as(:string) >> str("'")).as(:string) >> space? }
+        rule(:dq_string) { (str('"') >> ( str('\\') >> any | str('"').absnt? >> any ).repeat.as(:string) >> str('"')) >> space? }
+        rule(:sq_string) { (str("'") >> ( str('\\') >> any | str("'").absnt? >> any ).repeat.as(:string) >> str("'")) >> space? }
         rule(:string)    { dq_string | sq_string }
 
         rule(:property_string) { (str("{") >> ( str('\\') >> any | str("}").absnt? >> any ).repeat.as(:property) >> str("}")) >> space? }
@@ -44,21 +44,19 @@ module Pacer
 
         rule(:property) { identifier.as(:property) >> space? }
 
-        rule(:comparison) { (str('!=') | str('>=') | str('<=') | match("[=><]")) >> space? }
+        rule(:comparison) { (str('!=') | str('>=') | str('<=') | match("[=><]")).as(:op) >> space? }
         rule(:bool_and) { str('and') >> space? }
         rule(:bool_or) { str('or') >> space? }
         rule(:data) { variable | proc_variable | property | property_string | integer | float | string }
         rule(:negate) { str('not').as(:negate).maybe >> space? }
 
-        rule(:statement) { (negate >> ( data.as(:left) >> comparison.as(:op) >> data.as(:right) | proc_variable )).as(:statement) >> space? }
+        rule(:statement) { (negate >> ( data.as(:left) >> comparison >> data.as(:right) | proc_variable )).as(:statement) >> space? }
         rule(:group) { (negate >> lparen >> expression >> rparen).as(:group) >> space? }
         rule(:and_group) { ((statement | group) >> (bool_and >> (statement | group)).repeat(1) >> or_group.maybe).as(:and) }
         rule(:or_group) { ((bool_or >> (and_group | group | statement)).repeat(1)).as(:or) >> space? }
         rule(:expression) { (and_group | group | statement) >> or_group.maybe }
 
         root :expression
-
-        #a = b or (c = d and e = f)
       end
     end
   end
