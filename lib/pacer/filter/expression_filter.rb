4d5d38e5d9b0ca2eae3ab20dc39606c71ab0ba8b
Darrick Wiebe <darrick@innatesoftware.com>
2011-01-31 22:04:21 -0500

    Add most of the rest of the transform rules and restructure to eliminate some of that instance_eval BS.

--- lib/pacer/filter/expression_filter.rb
+++ lib/pacer/filter/expression_filter.rb
@@ -1,5 +1,11 @@
 require 'parslet'
 
+module Parslet
+  class Transform
+    public :simple, :sequence
+  end
+end
+
 module Pacer
   module Routes
     module RouteOperations
@@ -27,7 +33,7 @@ module Pacer
 
       remove_const 'Builder' if const_defined? 'Builder'
 
-      class Builder < Parslet::Transform
+      class Builder
         Filters = {
           '==' => com.tinkerpop.pipes.filter.ComparisonFilterPipe::Filter::EQUAL,
           '='  => com.tinkerpop.pipes.filter.ComparisonFilterPipe::Filter::EQUAL,
@@ -46,51 +52,91 @@ module Pacer
           '<=' => com.tinkerpop.pipes.filter.ComparisonFilterPipe::Filter::GREATER_THAN_EQUAL,
           '>=' => com.tinkerpop.pipes.filter.ComparisonFilterPipe::Filter::LESS_THAN_EQUAL
         }
-        rule(:str => simple(:x)) { x }
-        rule(:int => simple(:x)) { Integer(x) }
-        rule(:float => simple(:x)) { Float(x) }
-        rule(:bool => simple(:x)) { x == 'true' }
 
-        rule(:statement => { :left => { :prop => simple(:property) },
-                             :op => simple(:op),
-                             :right => simple(:value) }
-        ) do |h|
-          prop_pipe = com.tinkerpop.pipes.pgm.PropertyPipe.new(h[:property])
-          filter_pipe = com.tinkerpop.pipes.filter.ObjectFilterPipe.new(h[:value], Filters[h[:op]])
-          filter_pipe.setStarts(prop_pipe)
-          [prop_pipe, filter_pipe]
+        class << self
         end
 
-        rule(:statement => { :left => simple(:value),
-                             :op => simple(:op),
-                             :right => { :prop => simple(:property) } }
-        ) do |h|
-          prop_pipe = com.tinkerpop.pipes.pgm.PropertyPipe.new(h[:property])
-          filter_pipe = com.tinkerpop.pipes.filter.ObjectFilterPipe.new(h[:value], ReverseFilters[h[:op]])
-          filter_pipe.setStarts(prop_pipe)
-          [prop_pipe, filter_pipe]
+        def initialize(vars = {})
+          @vars = vars
+          @transform = t = Parslet::Transform.new
+          t.rule(:var => t.simple(:x)) { |h| @vars[h[:x]] }
+
+          t.rule(:str => t.simple(:x)) { x }
+          t.rule(:int => t.simple(:x)) { Integer(x) }
+          t.rule(:float => t.simple(:x)) { Float(x) }
+          t.rule(:bool => t.simple(:x)) { x == 'true' }
+
+          t.rule(:statement => { :left => { :prop => t.simple(:property) },
+                                 :op => t.simple(:op),
+                                 :right => t.simple(:value) }
+          ) do |h|
+            prop_pipe = com.tinkerpop.pipes.pgm.PropertyPipe.new(h[:property])
+            filter_pipe = com.tinkerpop.pipes.filter.ObjectFilterPipe.new(h[:value], Filters[h[:op]])
+            com.tinkerpop.pipes.Pipeline.new prop_pipe, filter_pipe
+          end
+
+          t.rule(:statement => { :left => { :prop => t.simple(:left) },
+                                 :op => t.simple(:op),
+                                 :right => { :prop => t.simple(:right) } }
+          ) do |h|
+            Pacer::Pipes::PropertyComparisonFilterPipe.new(h[:left], h[:right], Filters[h[:op]])
+          end
+          t.rule(:statement => { :left => t.simple(:value),
+                                 :op => t.simple(:op),
+                                 :right => { :prop => t.simple(:property) } }
+          ) do |h|
+            prop_pipe = com.tinkerpop.pipes.pgm.PropertyPipe.new(h[:property])
+            filter_pipe = com.tinkerpop.pipes.filter.ObjectFilterPipe.new(h[:value], ReverseFilters[h[:op]])
+            com.tinkerpop.pipes.Pipeline.new prop_pipe, filter_pipe
+          end
+
+          t.rule(:statement => { :left => t.simple(:left),
+                                 :op => t.simple(:op),
+                                 :right => t.simple(:right) }
+          ) do
+            result = case op
+            when '=', '==' ; left == right
+            when '>'       ; left > right
+            when '>='      ; left >= right
+            when '<'       ; left < right
+            when '<='      ; left <= right
+            when '!='      ; left != right
+            else
+              raise "Unrecognized operator #{ op }"
+            end
+            if result
+              Pacer::Pipes::TruePipe.new
+            else
+              Pacer::Pipes::FalsePipe.new
+            end
+          end
+
+          t.rule(:group => t.simple(:x)) { x }
+          t.rule(:group => t.simple(:group), :or => t.sequence(:or_pipes)) do |h|
+            g = OrGroup.new h[:or_pipes]
+            g.prepend h[:group]
+            g.pipe
+          end
+
+          t.rule(:or => t.sequence(:pipes)) do |h|
+            OrGroup.new h[:pipes]
+          end
+
+          t.rule(:and => t.sequence(:pipes)) do |h|
+            or_group = h[:pipes].pop if h[:pipes].last.is_a? OrGroup
+            and_pipe = com.tinkerpop.pipes.filter.AndFilterPipe.new *h[:pipes]
+            if or_group
+              or_group.prepend and_pipe
+              or_group.pipe
+            else
+              and_pipe
+            end
+          end
         end
 
-        rule(:statement => { :left => simple(:left),
-                             :op => simple(:op),
-                             :right => simple(:right) }
-        ) do
-          result = case op
-          when '=', '==' ; left == right
-          when '>'       ; left > right
-          when '>='      ; left >= right
-          when '<'       ; left < right
-          when '<='      ; left <= right
-          when '!='      ; left != right
-          else
-            raise "Unrecognized operator #{ op }"
-          end
-          if result
-            pipe = Pacer::Pipes::TruePipe.new
-          else
-            pipe = Pacer::Pipes::FalsePipe.new
-          end
-          [pipe, pipe]
+        def apply(tree, vars = nil)
+          @vars = vars if vars
+          @transform.apply tree
         end
       end
 
