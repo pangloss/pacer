0fb67dfd43960ef5077b23183a22499f75e7cd68
Darrick Wiebe <darrick@innatesoftware.com>
2011-04-03 22:59:54 -0400

    Move wrapper classes into their own files.

---
+++ lib/pacer/wrappers/edge_wrapper.rb
@@ -0,0 +1,35 @@
+module Pacer
+  class EdgeWrapper < ElementWrapper
+    include Pacer::Core::Graph::EdgesRoute
+    include ElementMixin
+    include EdgeMixin
+    include Comparable
+
+    def_delegators :@element,
+      :label, :get_label, :property_keys, :get_property, :set_property, :remove_property,
+      :in_vertex, :out_vertex,
+      :raw_edge,
+      :graph, :graph=, :<=>, :==
+
+    class << self
+      def wrapper_for(exts)
+        @wrappers ||= {}
+        @wrappers[exts] ||= build_edge_wrapper(exts)
+      end
+
+      protected
+
+      def build_edge_wrapper(exts)
+        build_extension_wrapper(exts, [:Route, :Edge], EdgeWrapper)
+      end
+    end
+
+    def extensions
+      self.class.extensions
+    end
+
+    def element
+      @element
+    end
+  end
+end
