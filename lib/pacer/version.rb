9d1e072f851ea9f31439736a907a53547e698737
Darrick Wiebe <darrick@innatesoftware.com>
2011-09-20 18:27:12 -0400

    Bump to 0.8.5 with blueprints 1.0 and pipes 0.8

--- lib/pacer/version.rb
+++ lib/pacer/version.rb
@@ -1,13 +1,13 @@
 module Pacer
   unless const_defined? :VERSION
-    VERSION = "0.8.4"
+    VERSION = "0.8.5"
 
     JAR = "pacer-#{ VERSION }-standalone.jar"
     JAR_PATH = "lib/#{ JAR }"
 
     START_TIME = Time.now
 
-    BLUEPRINTS_VERSION = "1.0-SNAPSHOT"
-    PIPES_VERSION = "0.8-SNAPSHOT"
+    BLUEPRINTS_VERSION = "1.0"
+    PIPES_VERSION = "0.8"
   end
 end
