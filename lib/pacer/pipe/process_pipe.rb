34b18ef3e1a2eaadc2f46a6389057775a7254137
Darrick Wiebe <darrick@innatesoftware.com>
2011-10-11 15:29:26 -0400

    I've needed this simple pipe for ages.

---
+++ lib/pacer/pipe/process_pipe.rb
@@ -0,0 +1,37 @@
+module Pacer::Pipes
+  class ProcessPipe2 < AbstractPipe
+    field_reader :starts
+    attr_reader :is_element, :extensions, :back, :block, :graph
+
+    def initialize(back, block)
+      super()
+      @block = block
+      @back = back
+      @graph = back.graph if back
+      @extensions = back.extensions + [Pacer::Extensions::BlockFilterElement]
+      @is_element = graph.element_type?(back.element_type) if graph
+    end
+
+    def processNextStart
+      while true
+        obj = starts.next
+        begin
+          if is_element
+            obj = obj.add_extensions(extensions)
+            obj.back = back
+            obj.graph = graph
+          end
+        rescue
+        end
+        result = block.call(obj)
+        return obj
+      end
+    rescue NativeException => e
+      if e.cause.getClass == Pacer::NoSuchElementException.getClass
+        raise e.cause
+      else
+        raise e
+      end
+    end
+  end
+end
