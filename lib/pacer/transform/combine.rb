9787e6bd5b337898ec41fe836f79e2538bc7d91d
Darrick Wiebe <darrick@innatesoftware.com>
2011-10-11 15:31:52 -0400

    Added a very cool fluent interface to group, subgroup and analyze data streams.

--- lib/pacer/transform/combine.rb
+++ lib/pacer/transform/combine.rb
@@ -11,6 +11,68 @@ module Pacer
         route = chain_route(args).route.join(options.fetch(:key, :key)) { |v| v }
         route.join(name, &block)
       end
+
+      def collect_with(multigraph)
+        vars[:multigraph] = multigraph
+      end
+
+      def collect_as(name, opts = {})
+        process do |element|
+          g = vars[:multigraph] ||= Pacer::MultiGraph.new
+          v = vars[name] = g.create_vertex
+          v[name] = element
+          within = opts[:within]
+          if within
+            within_v = vars[within]
+            g.create_edge nil, within_v, v, :within
+          end
+        end
+      end
+
+      def add_to(collection_name, name = nil)
+        process do |element|
+          v = vars[collection_name]
+          if name
+            existing = v[name]
+            if existing
+              existing << element
+            else
+              v[name] = [element]
+            end
+          end
+          if block_given?
+            yield v, element
+          end
+        end
+      end
+
+      def reduce_to(collection_name, name, starting_value)
+        process do |element|
+          v = vars[collection_name]
+          total = v[name]
+          if total
+            v[name] = yield total, element
+          else
+            v[name] = yield starting_value, element
+          end
+        end
+      end
+
+      def execute!
+        p = pipe
+        while p.hasNext
+          p.next
+        end
+        self.route
+      end
+
+      def collected(name = nil)
+        if name
+          map(graph: vars[:multigraph], element_type: :vertex) { vars[name] }
+        else
+          execute!.vars[:multigraph]
+        end
+      end
     end
   end
 
