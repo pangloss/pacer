2897d95a405e3e445ccf0cc2fbbfd3649265d21d
Darrick Wiebe <darrick@innatesoftware.com>
2011-10-20 20:59:31 -0400

    Remove Parslet and #expression.

--- lib/pacer/filter.rb
+++ lib/pacer/filter.rb
@@ -5,7 +5,6 @@ end
 
 require 'pacer/filter/collection_filter'
 require 'pacer/filter/empty_filter'
-require 'pacer/filter/expression_filter'
 require 'pacer/filter/future_filter'
 require 'pacer/filter/property_filter'
 require 'pacer/filter/range_filter'
