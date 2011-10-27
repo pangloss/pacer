8ca3ab5767aef18f9f5f7487c207164fbac8ceca
Darrick Wiebe <darrick@innatesoftware.com>
2011-10-27 16:37:12 -0400

    Brought back old branch code from before the branching pipes were initially axed from the Pipes project. This will need to be updated to match the new pipes and new Pacer structure but the old version worked well so I think it'll be a good base to start from.

---
+++ lib/pacer/route/mixin/branchable_route.rb
@@ -0,0 +1,21 @@
+module Pacer::Routes
+  module BranchableRoute
+    # Branch the route on a path defined within the given block. Call this
+    # method multiple times in a row to branch the route over different paths
+    # before merging back.
+    def branch(&block)
+      br = BranchedRoute.new(self, block)
+      if br.branch_count == 0
+        self
+      else
+        br
+      end
+    end
+
+    def source_branch(&block)
+      br = BranchedRoute.new(self)
+      br.source_branch(&block)
+      br
+    end
+  end
+end
