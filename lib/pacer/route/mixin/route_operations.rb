1d17e62cd1eb1e6c7f56299ab95732b6c0da3ac8
Darrick Wiebe <darrick@innatesoftware.com>
2011-10-27 02:11:46 -0400

    Put #random in its own module.

--- lib/pacer/route/mixin/route_operations.rb
+++ lib/pacer/route/mixin/route_operations.rb
@@ -5,14 +5,6 @@ module Pacer::Routes
   module RouteOperations
     include BulkOperations
 
-    # Return elements based on a bias:1 chance.
-    #
-    # If given an integer (n) > 0, bias is calcualated at 1 / n.
-    def random(bias = 0.5)
-      bias = 1 / bias.to_f if bias.is_a? Fixnum and bias > 0
-      chain_route :pipe_class => Pacer::Pipes::RandomFilterPipe, :pipe_args => bias
-    end
-
     def has?(element)
       any? { |e| e == element }
     end
