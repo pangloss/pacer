e3ab6136d80d0c6f4e1b8b26e7162e27414553f7
Darrick Wiebe <darrick@innatesoftware.com>
2011-10-27 20:46:43 -0400

    Hopefully this will fix the random filter test's random failures.

--- spec/pacer/filter/random_filter_spec.rb
+++ spec/pacer/filter/random_filter_spec.rb
@@ -7,9 +7,10 @@ Run.tg(:read_only) do
     describe '#random' do
       it { Set[*graph.v.random(1)].should == Set[*graph.v] }
       it { graph.v.random(0).to_a.should == [] }
-      it 'should have some number of elements more than 1 and less than all' do
+      it 'should usually have some number of elements more than 1 and less than all' do
         range = 1..(graph.v.count - 1)
-        range.should include(graph.v.random(0.5).count)
+        best2of3 = (0...3).map { graph.v.random(0.5).count }
+        best2of3.select { |num| range.include? num }.length.should >= 2
       end
     end
   end
