12afc891c219b6257e65c87e92101c35f791716c
Darrick Wiebe <darrick@innatesoftware.com>
2012-02-02 14:40:49 -0500

    Handle both_v in subgraph. Fixes #14.
    Adds 2 options to the clone edge method:
      :ignore_missing_vertices - if true will not raise an exception if an edge can't be created.
      :show_missing_vertices   - complains about missing vertices that prevent an edge from being created to stdout.

--- lib/pacer/transform/path.rb
+++ lib/pacer/transform/path.rb
@@ -15,17 +15,27 @@ module Pacer
         collect { |arraylist| arraylist.to_a }.transpose
       end
 
-      def subgraph(target_graph = nil)
+      def subgraph(target_graph = nil, opts = {})
         raise "Can't create a subgraph within itself." if target_graph == graph
         target_graph ||= Pacer.tg
         target_graph.vertex_name ||= graph.vertex_name
+        missing_edges = Set[]
         bulk_job(nil, target_graph) do |path|
           path_route = path.to_route(:graph => graph, :element_type => :mixed)
           path_route.v.each do |vertex|
             vertex.clone_into target_graph
           end
           path_route.e.each do |edge|
-            edge.clone_into target_graph
+            unless edge.clone_into target_graph, ignore_missing_vertices: true
+              missing_edges << edge
+            end
+          end
+        end
+        if missing_edges.any?
+          missing_edges.to_route(graph: graph, element_type: :edge).bulk_job nil, target_graph do |edge|
+            edge.clone_into target_graph,
+              ignore_missing_vertices: opts[:ignore_missing_vertices],
+              show_missing_vertices: opts[:show_missing_vertices]
           end
         end
         target_graph
