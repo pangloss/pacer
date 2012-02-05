a3e57f3f2ac61eb1b03bb9eb6ba89316283c76ce
Darrick Wiebe <darrick@innatesoftware.com>
2012-02-05 01:17:57 -0500

    Make sure #transpose and #subgraph methods still work on extended paths.

--- lib/pacer/transform/path.rb
+++ lib/pacer/transform/path.rb
@@ -5,7 +5,7 @@ module Pacer
         route = chain_route :transform => :path, :element_type => :object
         if exts.any?
           exts = exts.map { |e| Array.wrap(e) if e }
-          route.map do |path|
+          route.map(modules: Pacer::Transform::Path::Methods) do |path|
             path.zip(exts).map { |element, ext| ext ? element.add_extensions(ext) : element }
           end
         else
@@ -17,37 +17,41 @@ module Pacer
 
   module Transform
     module Path
-      import com.tinkerpop.pipes.transform.PathPipe
+      module Methods
+        def transpose
+          collect { |arraylist| arraylist.to_a }.transpose
+        end
 
-      def transpose
-        collect { |arraylist| arraylist.to_a }.transpose
-      end
-
-      def subgraph(target_graph = nil, opts = {})
-        raise "Can't create a subgraph within itself." if target_graph == graph
-        target_graph ||= Pacer.tg
-        target_graph.vertex_name ||= graph.vertex_name
-        missing_edges = Set[]
-        bulk_job(nil, target_graph) do |path|
-          path_route = path.to_route(:graph => graph, :element_type => :mixed)
-          path_route.v.each do |vertex|
-            vertex.clone_into target_graph
-          end
-          path_route.e.each do |edge|
-            unless edge.clone_into target_graph, ignore_missing_vertices: true
-              missing_edges << edge
+        def subgraph(target_graph = nil, opts = {})
+          raise "Can't create a subgraph within itself." if target_graph == graph
+          target_graph ||= Pacer.tg
+          target_graph.vertex_name ||= graph.vertex_name
+          missing_edges = Set[]
+          bulk_job(nil, target_graph) do |path|
+            path_route = path.to_route(:graph => graph, :element_type => :mixed)
+            path_route.v.each do |vertex|
+              vertex.clone_into target_graph
+            end
+            path_route.e.each do |edge|
+              unless edge.clone_into target_graph, ignore_missing_vertices: true
+                missing_edges << edge
+              end
             end
           end
-        end
-        if missing_edges.any?
-          missing_edges.to_route(graph: graph, element_type: :edge).bulk_job nil, target_graph do |edge|
-            edge.clone_into target_graph,
-              ignore_missing_vertices: opts[:ignore_missing_vertices],
-              show_missing_vertices: opts[:show_missing_vertices]
+          if missing_edges.any?
+            missing_edges.to_route(graph: graph, element_type: :edge).bulk_job nil, target_graph do |edge|
+              edge.clone_into target_graph,
+                ignore_missing_vertices: opts[:ignore_missing_vertices],
+                show_missing_vertices: opts[:show_missing_vertices]
+            end
           end
+          target_graph
         end
-        target_graph
       end
+
+      import com.tinkerpop.pipes.transform.PathPipe
+
+      include Methods
 
       protected
 
