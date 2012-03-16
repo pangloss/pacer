f478f81d982d944267e48204835699fc13f75dfa
Darrick Wiebe <dw@xnlogic.com>
2012-03-16 01:40:38 -0400

    Lots more tests and a suspect failing result

--- spec/pacer/filter/property_filter/filters_spec.rb
+++ spec/pacer/filter/property_filter/filters_spec.rb
@@ -1,59 +1,132 @@
 require 'spec_helper'
 
 module Pacer::Filter::PropertyFilter
-  describe Filters do
-    subject { filters }
 
-    context 'no properties' do
-      let(:filters) { Pacer::Route.filters [] }
+  Run.tg do
+    describe Filters do
+      subject { filters }
 
-      its(:any?) { should be_false }
-    end
+      context 'no properties' do
+        let(:filters) { Pacer::Route.filters [] }
 
-    context 'no properties' do
-      let(:filters) { Pacer::Route.filters [Tackle::SimpleMixin] }
+        its(:any?) { should be_false }
+      end
 
-      its(:any?) { should be_true }
-      its(:extensions) { should == [Tackle::SimpleMixin] }
-      its(:route_modules) { should be_empty }
-      its(:wrapper) { should be_nil }
-      its(:blocks) { should be_empty }
-      its(:properties) { should be_empty }
-    end
+      context 'no properties' do
+        let(:filters) { Pacer::Route.filters [Tackle::SimpleMixin] }
 
-    context 'simple properties' do
-      let(:filters) { Pacer::Route.filters([name: 'Darrick', nickname: 'pangloss']) }
+        its(:any?) { should be_true }
+        its(:extensions) { should == [Tackle::SimpleMixin] }
+        its(:route_modules) { should be_empty }
+        its(:wrapper) { should be_nil }
+        its(:blocks) { should be_empty }
+        its(:properties) { should be_empty }
+      end
 
-      its(:any?) { should be_true }
-      its(:extensions) { should be_empty }
-      its(:route_modules) { should be_empty }
-      its(:wrapper) { should be_nil }
-      its(:blocks) { should be_empty }
-      its(:properties) { should == [ %w[ name Darrick ], %w[ nickname pangloss ] ] }
-    end
+      context 'simple properties' do
+        let(:filters) { Pacer::Route.filters([name: 'Darrick', nickname: 'pangloss']) }
 
-    context 'with extensions' do
-      let(:filters) { Pacer::Route.filters([TP::Person, name: 'Darrick', nickname: 'pangloss']) }
+        its(:any?) { should be_true }
+        its(:extensions) { should be_empty }
+        its(:route_modules) { should be_empty }
+        its(:wrapper) { should be_nil }
+        its(:blocks) { should be_empty }
+        its(:properties) { should == [ %w[ name Darrick ], %w[ nickname pangloss ] ] }
+      end
 
-      its(:any?) { should be_true }
-      its(:extensions) { should == [TP::Person] }
-      its(:route_modules) { should be_empty }
-      its(:wrapper) { should be_nil }
-      its(:blocks) { should be_empty }
-      its(:properties) { should == [ %w[ type person ], %w[ name Darrick ], %w[ nickname pangloss ] ] }
-    end
+      context 'with extensions' do
+        let(:filters) { Pacer::Route.filters([TP::Person, name: 'Darrick', nickname: 'pangloss']) }
 
-    context 'with route module' do
-      # TODO: should this feature be removed?
-      let(:filters) { Pacer::Route.filters [TP::Pangloss] }
+        its(:any?) { should be_true }
+        its(:extensions) { should == [TP::Person] }
+        its(:route_modules) { should be_empty }
+        its(:wrapper) { should be_nil }
+        its(:blocks) { should be_empty }
+        its(:properties) { should == [ %w[ type person ], %w[ name Darrick ], %w[ nickname pangloss ] ] }
 
-      its(:any?) { should be_true }
-      its(:extensions) { should == [TP::Pangloss] }
-      its(:route_modules) { should == [TP::Pangloss] }
-      its(:wrapper) { should be_nil }
-      its(:blocks) { should be_empty }
-      its(:properties) { should be_empty }
+        context '+ indices' do
+          before do
+            filters.graph = graph
+            filters.indices = graph.getIndices
+            filters.choose_best_index = true
+            filters.search_manual_indices = true
+          end
 
+          def find_index
+            @idx, @key, @value = filters.best_index(:vertex)
+          end
+
+          it 'should use the automatic index' do
+            find_index
+            @idx.should be_a Java::ComTinkerpopBlueprintsPgmImplsTg::TinkerAutomaticIndex
+          end
+
+          it 'should use the first key and value' do
+            find_index
+            @key.should == 'type'
+            @value.should == 'person'
+          end
+
+          context '+ data' do
+            before do
+              5.times { graph.create_vertex type: 'person', name: 'Someone', nickname: nil }
+              graph.create_vertex type: 'person', name: 'Darrick', nickname: 'therealdarrick'
+            end
+
+            it 'should use the non-empty key when searching manual indices' do
+              find_index
+              @key.should == 'name'
+              @value.should == 'Darrick'
+            end
+
+            it 'should use the empty key' do
+              filters.search_manual_indices = false
+              find_index
+              @key.should == 'nickname'
+              @value.should == 'pangloss'
+            end
+
+            context '+ match' do
+              before do
+                3.times { graph.create_vertex type: 'person', name: 'Immitator', nickname: 'pangloss' }
+              end
+
+              let!(:match) do
+                graph.create_vertex type: 'person', name: 'Darrick', nickname: 'pangloss'
+              end
+
+              it 'should use the smallest key' do
+                find_index
+                @key.should == 'name'
+                @value.should == 'Darrick'
+              end
+            end
+          end
+        end
+      end
+
+      context 'with route module' do
+        # TODO: should this feature be removed?
+        let(:filters) { Pacer::Route.filters [TP::Pangloss] }
+
+        its(:any?) { should be_true }
+        its(:extensions) { should == [TP::Pangloss] }
+        its(:route_modules) { should == [TP::Pangloss] }
+        its(:wrapper) { should be_nil }
+        its(:blocks) { should be_empty }
+        its(:properties) { should be_empty }
+      end
+
+      context 'with manual index' do
+        let(:filters) { Pacer::Route.filters [tokens: { short: '555555' }, name: 'Darrick'] }
+
+        its(:any?) { should be_true }
+        its(:extensions) { should be_empty }
+        its(:route_modules) { should be_empty }
+        its(:wrapper) { should be_nil }
+        its(:blocks) { should be_empty }
+        its(:properties) { should == [%w[ name Darrick ]] }
+      end
     end
   end
 end
