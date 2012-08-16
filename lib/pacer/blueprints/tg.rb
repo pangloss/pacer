6978b47c616d19dc412269a507bc1be72b02df87
Darrick Wiebe <dw@xnlogic.com>
2012-08-15 23:32:16 -0400

    Remove extensions, etc from TinkerElements.

--- lib/pacer/blueprints/tg.rb
+++ lib/pacer/blueprints/tg.rb
@@ -1,8 +1,5 @@
 module Pacer
   import com.tinkerpop.blueprints.impls.tg.TinkerGraph
-  import com.tinkerpop.blueprints.impls.tg.TinkerVertex
-  import com.tinkerpop.blueprints.impls.tg.TinkerEdge
-  import com.tinkerpop.blueprints.impls.tg.TinkerElement
   import com.tinkerpop.blueprints.impls.tg.TinkerIndex
 
   # Create a new TinkerGraph. If path is given, use Tinkergraph in
@@ -17,45 +14,5 @@ module Pacer
 
   class TinkerIndex
     include IndexMixin
-  end
-
-
-  # Extend the java class imported from blueprints.
-  class TinkerVertex
-    include Pacer::Core::Graph::VerticesRoute
-    include ElementMixin
-    include VertexMixin
-  end
-
-
-  # Extend the java class imported from blueprints.
-  class TinkerEdge
-    include Pacer::Core::Graph::EdgesRoute
-    include ElementMixin
-    include EdgeMixin
-
-    def in_vertex(extensions = nil)
-      v = getVertex Pacer::Pipes::IN
-      v.graph = graph
-      if extensions.is_a? Enumerable
-        v.add_extensions extensions
-      elsif extensions
-        v.add_extensions [extensions]
-      else
-        v
-      end
-    end
-
-    def out_vertex(extensions = nil)
-      v = getVertex Pacer::Pipes::OUT
-      v.graph = graph
-      if extensions.is_a? Enumerable
-        v.add_extensions extensions
-      elsif extensions
-        v.add_extensions [extensions]
-      else
-        v
-      end
-    end
   end
 end
