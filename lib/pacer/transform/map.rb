bda63e5b2237fdaf68d9dcbc2fdb40975f1aa02c
Darrick Wiebe <darrick@innatesoftware.com>
2012-08-28 19:32:09 -0400

    Replaced MapPipe with the pipes version.

--- lib/pacer/transform/map.rb
+++ lib/pacer/transform/map.rb
@@ -14,7 +14,9 @@ module Pacer
       protected
 
       def attach_pipe(end_pipe)
-        pipe = Pacer::Pipes::MapPipe.new(back, block)
+        pf = Pacer::Wrappers::WrappingPipeFunction.new self, block
+        pf = Pacer::Wrappers::UnwrappingPipeFunction.new pf
+        pipe = com.tinkerpop.pipes.transform.TransformFunctionPipe.new pf
         pipe.setStarts end_pipe if end_pipe
         pipe
       end
