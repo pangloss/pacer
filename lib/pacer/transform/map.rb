pangloss/pacer

commit bda63e5b2237fdaf68d9dcbc2fdb40975f1aa02c

Replaced MapPipe with the pipes version.

lib/pacer/transform/map.rb
@@ -14,7 +14,9 @@ module Pacer
       protected
 
       def attach_pipe(end_pipe)
-        pipe = Pacer::Pipes::MapPipe.new(back, block)
+        pf = Pacer::Wrappers::WrappingPipeFunction.new self, block
+        pf = Pacer::Wrappers::UnwrappingPipeFunction.new pf
+        pipe = com.tinkerpop.pipes.transform.TransformFunctionPipe.new pf
         pipe.setStarts end_pipe if end_pipe
         pipe
       end

lib/pacer/pipe/map_pipe.rb
@@ -1,20 +1,0 @@
-module Pacer::Pipes
-  class MapPipe < AbstractPipe
-    field_reader :starts
-
-    attr_reader :block
-
-    def initialize(back, block)
-      super()
-      @block = Pacer::Wrappers::WrappingPipeFunction.new back, block
-      @block = Pacer::Wrappers::UnwrappingPipeFunction.new @block
-    end
-
-    def processNextStart
-      while true
-        obj = starts.next
-        return block.call(obj)
-      end
-    end
-  end
-end

lib/pacer/pipes.rb
@@ -42,7 +42,6 @@ require 'pacer/pipe/collection_filter_pipe'
 require 'pacer/pipe/enumerable_pipe'
 require 'pacer/pipe/expandable_pipe'
 require 'pacer/pipe/loop_pipe'
-require 'pacer/pipe/map_pipe'
 require 'pacer/pipe/process_pipe'
 require 'pacer/pipe/visitor_pipe'
 require 'pacer/pipe/simple_visitor_pipe'

commit 923f8379d91284c2d53dec0aa0430bc81de49498

Fixed #map using wrong extensions.
Caused by bda63e5b

Make wrapping pipe handle no back object (for map directly from a source) and fix map to use back again.

lib/pacer/transform/map.rb
@@ -14,7 +14,9 @@ module Pacer
       protected
 
       def attach_pipe(end_pipe)
-        pf = Pacer::Wrappers::WrappingPipeFunction.new self, block
+        # Must wrap based on parent pipe because the element in the block has
+        # not yet been affected by any of this block's transforms.
+        pf = Pacer::Wrappers::WrappingPipeFunction.new back, block
         pf = Pacer::Wrappers::UnwrappingPipeFunction.new pf
         pipe = com.tinkerpop.pipes.transform.TransformFunctionPipe.new pf
         pipe.setStarts end_pipe if end_pipe

lib/pacer/wrappers/wrapping_pipe_function.rb
@@ -8,9 +8,12 @@ module Pacer
       def initialize(back, block)
         @back = back
         @block = block
-        @graph = back.graph
-        @extensions = back.extensions + [Pacer::Extensions::BlockFilterElement]
-        @wrapper = WrapperSelector.build back.element_type, extensions
+        if back
+          @graph = back.graph
+          @extensions = back.extensions + [Pacer::Extensions::BlockFilterElement]
+          element_type = back.element_type
+        end
+        @wrapper = WrapperSelector.build element_type, extensions
       end
 
       def arity