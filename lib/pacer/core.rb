e77816ecf6beb1a06f785c174f6ba4d61268a0d9
Darrick Wiebe <dw@xnlogic.com>
2012-09-21 02:01:13 -0400

    Consolidate all of the files that just do loading.

--- lib/pacer/core.rb
+++
@@ -1,8 +0,0 @@
-module Pacer
-  module Core
-  end
-end
-
-require 'pacer/core/route'
-require 'pacer/core/graph'
-require 'pacer/core/side_effect'
--- lib/pacer/routes.rb
+++
@@ -1,6 +0,0 @@
-module Pacer
-  module Routes
-    # Defines the Routes namespace
-  end
-end
-require 'pacer/route/mixins'
