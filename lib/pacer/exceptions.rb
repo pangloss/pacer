dfea287c10217bc38ffb9f6c369ea8e14da0f936
Darrick Wiebe <dw@xnlogic.com>
2012-11-20 17:25:27 -0500

    Tweak Exception hierarchy.

--- lib/pacer/exceptions.rb
+++ lib/pacer/exceptions.rb
@@ -6,11 +6,12 @@ module Pacer
 
     class LogicError < Error; end
       class ClientError < LogicError; end
-        class TransactionConcludedError < ClientError; end
-        class NestedTransactionError < ClientError; end
-        class NestedTransactionRollback < ClientError; end
-        class NestedMockTransactionRollback < NestedTransactionRollback; end
-        class MockTransactionRollback < ClientError; end
+        class TransactionError < ClientError; end
+          class TransactionConcludedError < TransactionError; end
+          class NestedTransactionError < TransactionError; end
+          class NestedTransactionRollback < TransactionError; end
+          class NestedMockTransactionRollback < NestedTransactionRollback; end
+          class MockTransactionRollback < TransactionError; end
         class UnsupportedOperation < ClientError; end
 
       class InternalError < LogicError; end
