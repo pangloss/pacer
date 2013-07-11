94189196d891163a466f8508740089bc2bcadb98
purbon <pere.urbon@gmail.com>
2013-07-11 14:40:42 +0200

    Add a filter so whenever we receive an element that can be unwrapped inside the object filter pipe, this fix bug #40

--- spec/pacer/filter/object_filter_spec.rb
+++ spec/pacer/filter/object_filter_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 Run.tg(:read_only) do
   use_pacer_graphml_data(:read_only)
-
+  
   describe Pacer::Filter::ObjectFilter do
     it '#is' do
       [1, 2, 3, 2, 3].to_route.is(2).to_a.should == [2, 2]
@@ -18,14 +18,14 @@ Run.tg(:read_only) do
 
     describe '#vertex filter' do
 
-      let (:filter_node) { graph.v.first }
+      let (:filter_node) { graph.vertex 3 }
     
-      it "#is" do
+      it "#is_not" do
         all_nodes = graph.v.to_a.select { |n| n.getId != filter_node.getId }
         graph.v.is_not(filter_node).to_a.should == all_nodes
       end
 
-      it "#is_not" do
+      it "#is" do
         graph.v.is(filter_node).to_a.should == [filter_node]
       end
 
