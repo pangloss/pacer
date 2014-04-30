04fde98cc52f013544f3ef07ab78946398440e0d
Darrick Wiebe <dw@xnlogic.com>
2014-04-30 14:17:26 -0400

    Update to use released Tinkerpop jars

--- lib/pacer/version.rb
+++ lib/pacer/version.rb
@@ -1,13 +1,13 @@
 module Pacer
   unless const_defined? :VERSION
-    VERSION = "1.5.0.pre"
+    VERSION = "1.5.0"
 
     JAR = "pacer-#{ VERSION }-standalone.jar"
     JAR_PATH = "lib/#{ JAR }"
 
     START_TIME = Time.now
 
-    BLUEPRINTS_VERSION = "2.5.0-SNAPSHOT"
-    PIPES_VERSION = "2.5.0-SNAPSHOT"
+    BLUEPRINTS_VERSION = "2.5.0"
+    PIPES_VERSION = "2.5.0"
   end
 end
