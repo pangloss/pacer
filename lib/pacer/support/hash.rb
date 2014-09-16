e097931deef0c7bb7b17c664bc59443bd94ad50a
Darrick Wiebe <dw@xnlogic.com>
2014-09-16 18:07:36 -0400

    better to_hash_map

--- lib/pacer/support/hash.rb
+++ lib/pacer/support/hash.rb
@@ -1,8 +1,14 @@
 class Hash
   def to_hash_map
     hm = java.util.HashMap.new
-    each do |key, value|
-      hm.put key, value
+    if block_given?
+      each do |key, value|
+        hm.put(*yield(key, value))
+      end
+    else
+      each do |key, value|
+        hm.put key, value
+      end
     end
     hm
   end
