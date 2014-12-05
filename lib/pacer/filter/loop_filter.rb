eb7fd16e976b43d93a35f2957e57d041647e70d9
Darrick Wiebe <dw@xnlogic.com>
2014-12-04 22:59:01 -0800

    Use the faster Pipes LoopPipe for #all

--- lib/pacer/filter/loop_filter.rb
+++ lib/pacer/filter/loop_filter.rb
@@ -2,18 +2,7 @@ module Pacer
   module Routes
     module RouteOperations
       def loop(opts = {}, &block)
-        chain_route(opts.merge :filter => :loop, :looping_route => block)
-      end
-
-      def all(opts = {}, &block)
-        include_self = opts[:include_self]
-        loop(opts, &block).while do |e, depth|
-          if depth == 0 and not include_self
-            :loop
-          else
-            :loop_and_recur
-          end
-        end
+        chain_route(opts.merge :filter => Pacer::Filter::LoopFilter, :looping_route => block)
       end
 
       def deepest(&block)
