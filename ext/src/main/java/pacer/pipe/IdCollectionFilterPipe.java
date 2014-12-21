e3c300e3260f61436d108eb5f195e0889ecf9301
Duncan Brown <db@xnlogic.com>
2014-12-21 00:47:50 -0500

    First pass at IdCollectionFilterPipeTest with tests. Need to review TODOs in this with dw.

---
+++ ext/src/main/java/pacer/pipe/IdCollectionFilterPipe.java
@@ -0,0 +1,46 @@
+package com.xnlogic.pacer.pipes;
+
+import com.tinkerpop.blueprints.Contains;
+import com.tinkerpop.blueprints.Element;
+import com.tinkerpop.pipes.AbstractPipe;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Arrays;
+
+public class IdCollectionFilterPipe<E extends Element> extends AbstractPipe<E, E> {
+    private Set ids;
+    private boolean containsIn;
+    
+    // TODO: Consider making this a derived exception.  Also, this constructor is the reverse of the Ruby one. Is this ok?
+    // Also also, is an Object array the way to go?  Can we nail down the type further?
+    public IdCollectionFilterPipe(final Contains comparison, final Object... ids) throws RuntimeException {
+        super();
+        this.ids = new HashSet();
+        this.ids.addAll(Arrays.asList(ids));
+
+        if (comparison == Contains.IN)
+            this.containsIn = true;
+        else if (comparison == Contains.NOT_IN)
+            this.containsIn = false;
+        else
+            throw new RuntimeException("Unknown comparison type for ID collection filter");
+    }
+
+    protected E processNextStart() {
+        if (this.containsIn) {
+            while (true) {
+                E e = this.starts.next();
+
+                if (e != null && this.ids.contains(e.getId())) 
+                    return e;
+            }
+        } else {
+            while (true) {
+                E e = this.starts.next();
+
+                if (e != null && !this.ids.contains(e.getId()))
+                    return e;
+            }
+        }
+    }
+}
