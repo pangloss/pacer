5fcaa7139cc3eb833446b986fd9f024c251c0028
Darrick Wiebe <dw@xnlogic.com>
2015-05-03 16:08:57 -0400

    [skip ci] New development cycle with version 2.0.16.pre

--- lib/pacer/version.rb
+++ lib/pacer/version.rb
@@ -1,5 +1,5 @@
 module Pacer
-  VERSION = "2.0.15"
+  VERSION = "2.0.16.pre"
   # Clients may optionally define the following constants in the Pacer namespace:
   # - LOAD_JARS : set to false to manage jar loading in the client. Be sure to load the jars defined in JARFILES.
   # - LOCKJAR_LOCK_OPTS : set some options to be passed to LockJar.lock (ie. :lockfile, :download_artifacts, :local_repo)
